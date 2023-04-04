b415a816990f1a32cd6a8f9ba63f38c9c21e1d3e
Lion <murikmusaev86@gmail.com>
2023-04-04 22:56:08 +0300

    Initial3

--- src/test/kotlin/ru/netology/PostTest.kt
+++ src/test/kotlin/ru/netology/PostTest.kt
@@ -10,17 +10,17 @@ class PostTest {
 
     @Test
     fun add1() {
-        val result = WallService.add(Post(0, 23, 24, 26, "Murat Musaev", "Первый пост", 23,CommentsPost(), 23, false, true, false, 34, "post", 23, true, true,false, true, 23, donut(), geo(), copyrRight(), likes(),Views23(), Reposts()))
+        val result = WallService.add(Post(0, 23, 24, 26, "Murat Musaev", "Первый пост", 23,CommentsPost(), 23, false, true, false, 34, "post", 23, true, true,false, true, 23, Donut(), Geo(), copyrRight(), likes(),Views23(), Reposts()))
         assertEquals(1, result.id)
     }
 
     @Test
     fun update3() {
         val service2 = WallService
-        service2.add(Post(0,2,5, 5, "sdada", "Vehsas", 23, CommentsPost(), 23, true, false, true, 55, "rfr ltkf" , 54, false, true, false, null,444, donut(), geo(), copyrRight(), likes(),Views23(), Reposts() ))
-        service2.add(Post(1,2,5, 5, "sdada", "Vehsas", 23, CommentsPost(), 23, true, false, true, 55, "rfr ltkf" , 54, false, true, false, null,444, donut(), geo(), copyrRight(), likes(),Views23(), Reposts() ))
-        service2.add(Post(3,2,5, 5, "sdada", "Vehsas", 23, CommentsPost(), 23, true, false, true, 55, "rfr ltkf" , 54, false, true, false, null,444, donut(), geo(), copyrRight(), likes(),Views23(), Reposts() ))
-        var update = Post(3,2,5, 5, "sdada", "Vehsas", 23, CommentsPost(), 23, true, false, true, 55, "rfr ltkf" , 54, false, true, false, null,444, donut(), geo(), copyrRight(), likes(),Views23(), Reposts() )
+        service2.add(Post(0,2,5, 5, "sdada", "Vehsas", 23, CommentsPost(), 23, true, false, true, 55, "rfr ltkf" , 54, false, true, false, null,444, Donut(), Geo(), copyrRight(), likes(),Views23(), Reposts() ))
+        service2.add(Post(1,2,5, 5, "sdada", "Vehsas", 23, CommentsPost(), 23, true, false, true, 55, "rfr ltkf" , 54, false, true, false, null,444, Donut(), Geo(), copyrRight(), likes(),Views23(), Reposts() ))
+        service2.add(Post(3,2,5, 5, "sdada", "Vehsas", 23, CommentsPost(), 23, true, false, true, 55, "rfr ltkf" , 54, false, true, false, null,444, Donut(), Geo(), copyrRight(), likes(),Views23(), Reposts() ))
+        var update = Post(3,2,5, 5, "sdada", "Vehsas", 23, CommentsPost(), 23, true, false, true, 55, "rfr ltkf" , 54, false, true, false, null,444, Donut(), Geo(), copyrRight(), likes(),Views23(), Reposts() )
 
         val result = service2.update(update)
 
@@ -31,13 +31,25 @@ class PostTest {
     @Test
     fun update2() {
         val service2 = WallService
-        service2.add(Post(0,2,5, 5, "sdada", "Vehsas", 23, CommentsPost(), 23, true, false, true, 55, "rfr ltkf" , 54, false, true, false, null,444, donut(), geo(), copyrRight(), likes(),Views23(), Reposts() ))
-        service2.add(Post(1,2,5, 5, "sdada", "Vehsas", 23, CommentsPost(), 23, true, false, true, 55, "rfr ltkf" , 54, false, true, false, null,444, donut(), geo(), copyrRight(), likes(),Views23(), Reposts() ))
-        service2.add(Post(3,2,5, 5, "sdada", "Vehsas", 23, CommentsPost(), 23, true, false, true, 55, "rfr ltkf" , 54, false, true, false, null,444, donut(), geo(), copyrRight(), likes(),Views23(), Reposts() ))
-        var update = Post(4,2,5, 5, "sdada", "Vehsas", 23, CommentsPost(), 23, true, false, true, 55, "rfr ltkf" , 54, false, true, false, null,444, donut(), geo(), copyrRight(), likes(),Views23(), Reposts() )
+        service2.add(Post(0,2,5, 5, "sdada", "Vehsas", 23, CommentsPost(), 23, true, false, true, 55, "rfr ltkf" , 54, false, true, false, null,444, Donut(), Geo(), copyrRight(), likes(),Views23(), Reposts() ))
+        service2.add(Post(1,2,5, 5, "sdada", "Vehsas", 23, CommentsPost(), 23, true, false, true, 55, "rfr ltkf" , 54, false, true, false, null,444, Donut(), Geo(), copyrRight(), likes(),Views23(), Reposts() ))
+        service2.add(Post(3,2,5, 5, "sdada", "Vehsas", 23, CommentsPost(), 23, true, false, true, 55, "rfr ltkf" , 54, false, true, false, null,444, Donut(), Geo(), copyrRight(), likes(),Views23(), Reposts() ))
+        var update = Post(4,2,5, 5, "sdada", "Vehsas", 23, CommentsPost(), 23, true, false, true, 55, "rfr ltkf" , 54, false, true, false, null,444, Donut(), Geo(), copyrRight(), likes(),Views23(), Reposts() )
         val result = service2.update(update)
 
         assertFalse(result)
+    }
+
+    @Test
+    fun addCom() {
+        WallService.add(Post(0, 23, 24, 26, "Murat Musaev", "Первый пост", 23,CommentsPost(), 23, false, true, false, 34, "post", 23, true, true,false, true, 23, Donut(), Geo(), copyrRight(), likes(),Views23(), Reposts()))
+        val resultComment = WallService.createComment(1, Comment(0, "gas"))
+        assertEquals(1, resultComment.id)
+    }
+
+    @Test(expected = PostNotFoundException::class)
+    fun addComExc() {
+        WallService.createComment(5,Comment(5,"54654"))
     }
     @Before
     fun clearBeforeTest() {
